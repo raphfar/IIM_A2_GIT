f480b7ae9d0e0c6178ac3baa3fdd84f2cc869e27
Anthony bodin <anthony.bodin@devinci.fr>
2016-09-16 11:05:37 +0200

    add music pute

--- add_music.php
+++ add_music.php
@@ -8,17 +8,28 @@ if( isset($_FILES['music']) && !empty($_FILES['music']) &&
 	
 	$file = $_FILES['music'];
 
-	// Si le "fichier" reçu est bien un fichier
+	if (is_file($destination)){
+
+			// Si le "fichier" reçu est bien un fichier
 		$ext = strtolower(substr(strrchr($file['name'], '.')  ,1));
 		// Vérification des extentions
 		if (preg_match('/\.(mp3|ogg)$/i', $file['name'])) {
 			$filename = md5(uniqid(rand(), true));
 			$destination = "musics/{$filename}.{$_SESSION['id']}.{$ext}";
  
+			exec('INSERT INTO IIM_GIT_SoundCloud(title,file) VALUES($filename,$destination)');
+
 		} else {
 			$error = 'Erreur, le fichier n\'a pas une extension autorisée !';
 		}
 	// }
+
+	}
+
+	else{
+		$error = 'Erreur, le fichier n\'est pas un véritable fichier !';
+	}
+
 }
 
 include 'view/_header.php';
